f69eb431ce9278a5ff270d8e1ca73907e32f3fd0
Bakmurat <bakmuratkubanaliev@gmail.com>
2025-04-26 14:26:27 -0700

    route tables added

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -26,3 +26,44 @@ resource "aws_subnet" "fp-priv-sunbets" {
     Name = "${var.project_name}-priv-subnet-${count.index + 1}"
   }
 }
+
+resource "aws_internet_gateway" "fp-igw" {
+  vpc_id = aws_vpc.fp-vpc.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+resource "aws_route_table" "fp-pub-rt" {
+  vpc_id = aws_vpc.fp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.fp-igw.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-pub-rt"
+  }
+}
+
+resource "aws_route_table" "fp-priv-rt" {
+  vpc_id = aws_vpc.fp-vpc.id
+
+  tags = {
+    Name = "${var.project_name}-priv-rt"
+  }
+}
+
+resource "aws_route_table_association" "fp-pub-subs-rt-association" {
+  count          = length(var.pub_subnets_cidr)
+  subnet_id      = aws_subnet.fp-pub-sunbets[count.index].id
+  route_table_id = aws_route_table.fp-pub-rt.id
+}
+
+resource "aws_route_table_association" "fp-priv-subs-rt-association" {
+  count          = length(var.priv_subnets_cidr)
+  subnet_id      = aws_subnet.fp-priv-sunbets[count.index].id
+  route_table_id = aws_route_table.fp-priv-rt.id
+}
